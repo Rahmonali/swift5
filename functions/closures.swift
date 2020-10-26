25435242fc3daa7ef8fe35cbba328e897cea2ac6
Rahmonali <rahmonali1995@gmail.com>
2020-10-26 09:03:27 +0500

    Update closures.swift

--- functions/closures.swift
+++ functions/closures.swift
@@ -26,3 +26,38 @@ var multiplyByThree = { base in
 
 multiplyByThree(3)
 
+/************/
+func filterLessThenFive(value : Int, numbers: [Int]) -> [Int]{
+    var filteredSetOfNumber = [Int]()
+    for num in numbers {
+        if num > value {
+            filteredSetOfNumber.append(num)
+        }
+    }
+    return filteredSetOfNumber
+}
+var result = filterLessThenFive(value: 2, numbers: [1,2,3,4,5,6,7,11])
+print(result)
+
+/************/
+func filteredWithClosure(closure: (Int)-> Bool, number: [Int]) -> [Int]{
+    var filteredNumber = [Int]()
+    
+    for num in number{
+        if closure(num){
+            filteredNumber.append(num)
+        }
+    }
+    return filteredNumber
+
+}
+
+func greaterThanThree(value: Int) -> Bool {
+    return value > 3
+}
+
+var numberArray = [77,88,1,2,3,4,5,6,7,8,9,0]
+
+let filteredList = filteredWithClosure(closure: greaterThanThree, number: numberArray)
+
+print(filteredList.sorted(by: <))
