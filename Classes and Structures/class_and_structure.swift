58754aa3b90c471802a1409a998ffcbae31ed3d7
Rahmonali <rahmonali1995@gmail.com>
2020-10-28 16:30:21 +0500

    Create class_and_structure.swift

---
+++ Classes and Structures/class_and_structure.swift
@@ -0,0 +1,15 @@
+
+/**
+
+ A class is a reference type & a struct is a value type
+
+**/
+
+class SomeClass{
+  // class definition goes here
+}
+
+struct SomeStructure{
+  // structure definition goes here
+}
+
